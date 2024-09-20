bf3400f68c1fcf90f5bfef5468cd67338579d143
Sousa1302 <sousaraposo1302@gmail.com>
2024-09-20 17:16:44 +0000

    Menu fixed

--- Sistema_Produtos/code.cpp
+++ Sistema_Produtos/code.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -10,15 +11,18 @@ struct Produto
 };
 
 Produto items[] = {};
-int qunatidade = 0;
+int qunatidade = 1;
 
 void adicionarProduto(Produto produtos[], int& quantidadeAtual){
-    cout << "Nome: ";
-    cin >> produtos->nome;
-    cout << "Preco: ";
-    cin >> produtos->preco;
-    cout << "Quantidade: ";
-    cin >> produtos->quantidade;
+    //mudar a condicao
+    for(int x = 0; x < quantidadeAtual ; x++){
+        cout << "Nome: ";
+        cin >> produtos->nome;
+        cout << "Preco: ";
+        cin >> produtos->preco;
+        cout << "Quantidade: ";
+        cin >> produtos->quantidade;
+    }
 
     // ? Produto new_product_1;
 }
@@ -47,23 +51,20 @@ void menu(){
         cout << "1. Adicionar Produtor \n2. Exibir Produtos \n3. Calcular Valor Total do Stock \n4. Sair \n";
         cin >> menu_choice;
 
-        switch (menu_choice)
-        {
-        case 0:
-            break;
-        case 1:
+        if(menu_choice == 1){
             adicionarProduto(items, qunatidade);
-            break;
-        case 2:
-
-            break;
-        case 3:
-
-            break;
-        default:
-            cout << "Escolhe uma opção !\n";
+        }
+        else if (menu_choice == 2){
+            exibirProduto(items, qunatidade);
+        }
+        else if(menu_choice == 3){
+            calcularValorTotal(items, qunatidade);
+        }
+        else if(menu_choice == 4){
+            system("clear");
             break;
         }
+        
     }while(menu_choice > 0 || menu_choice < 5);
 
 }
--- Sistema_Produtos/tempCodeRunnerFile.cpp
+++ Sistema_Produtos/tempCodeRunnerFile.cpp
@@ -1,17 +1,1 @@
-    switch (menu_choice)
-        {
-        case 0:
-            break;
-        case 1:
-            adicionarProduto(items, qunatidade);
-            break;
-        case 2:
-
-            break;
-        case 3:
-
-            break;
-        default:
-            cout << "Escolhe uma opção !\n";
-            break;
-        }
+system("clear");
